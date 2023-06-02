5bfad250a56c438e3a367349a14dca43f68f86db
vgvishesh <vgvishesh2008@gmail.com>
2023-06-02 19:17:25 +0530

    adding color changer

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 let button = document.getElementById('changeBackground');
+let count = 0;
+let colors = ['seagreen', 'fuchsia', 'white']
 button.addEventListener('click', () => {
-
+  const index = count++ % 3;
+  console.log(index);
+  document.body.style.backgroundColor = colors[index];
 })
