33c9a684c17b4bf80485e4f7348cc9ee0e8ab084
vgvishesh <vgvishesh2008@gmail.com>
2023-06-02 19:18:56 +0530

    cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,5 @@ let count = 0;
 let colors = ['seagreen', 'fuchsia', 'white']
 button.addEventListener('click', () => {
   const index = count++ % 3;
-  console.log(index);
   document.body.style.backgroundColor = colors[index];
 })
