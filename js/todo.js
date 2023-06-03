a49ddd768dd9fa619ace8526f677f2e4cef61ef4
vgvishesh <vgvishesh2008@gmail.com>
2023-06-03 15:38:28 +0530

    adding more interaction

--- js/todo.js
+++ js/todo.js
@@ -11,7 +11,18 @@ impact.addEventListener('click', () => {
   const text = 'hello world'
   newTodo.textContent = `${text} ${count++}`
   newTodo.addEventListener('click', () => {
-    todoList.removeChild(newTodo);
+    // todoList.removeChild(newTodo);
+    const newDiv = document.createElement('div');
+    newDiv.style.height = '50px';
+    newDiv.style.border = '1px solid green';
+    newDiv.textContent = `this is year ${count++}`;
+
+    const sib = newTodo.children[0];
+    if (sib) {
+      newTodo.replaceChild(newDiv, sib);
+    } else {
+      newTodo.appendChild(newDiv);
+    }
   })
   // Add new todo to the end of the list 
   todoList.appendChild(newTodo);
