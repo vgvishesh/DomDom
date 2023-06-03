1ba6aabf54069708c1b2e34943321e0945eb2352
vgvishesh <vgvishesh2008@gmail.com>
2023-06-03 16:55:55 +0530

    playing with styles

---
+++ js/style.js
@@ -0,0 +1,9 @@
+const div = document.querySelector('div'); // Apply style to div
+// div.setAttribute('style', 'text-align: center');
+div.style.height = '100px';
+div.style.width = '100px';
+div.style.border = '2px solid black';
+div.style.borderRadius = '50%';
+div.style.display = 'flex';
+div.style.justifyContent = 'center';
+div.style.alignItems = 'center';
