d97af55ef9ab8aa4669be306a84aea6affb9c26d
vgvishesh <vgvishesh2008@gmail.com>
2023-06-06 04:00:22 +0530

    using event properties to handle events

--- js/events.js
+++ js/events.js
@@ -1,4 +1,6 @@
 const changeText = () => {
   const p = document.querySelector('p');
-  p.textContent = "I changed because of an inline event handler.";
+  p.textContent = "I changed because of an event handler property.";
 }
+// Add event handler as a property of the button element
+const button = document.querySelector('button'); button.onclick = changeText;
