61bd479e1d791985643fdeeb7d2f09cfd0ce6bbe
vgvishesh <vgvishesh2008@gmail.com>
2023-06-06 04:02:24 +0530

    oveEkJagahr

--- js/events.js
+++ js/events.js
@@ -1,6 +1,11 @@
+const p = document.querySelector('p');
+const button = document.querySelector('button');
 const changeText = () => {
-  const p = document.querySelector('p');
-  p.textContent = "I changed because of an event handler property.";
+  p.textContent = "Will I change?";
 }
-// Add event handler as a property of the button element
-const button = document.querySelector('button'); button.onclick = changeText;
+const alertText = () => {
+  alert('Will I alert?');
+}
+// Events can be overwritten
+button.onclick = changeText;
+button.onclick = alertText;
